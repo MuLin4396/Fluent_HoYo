bb10aabefb56b647b33266f3ac49050609f99ed0
MuLin4396 <ming439611@outlook.com>
2024-08-18 19:40:11 +0800

    Add 增加临时！！！！！

--- Config/Config.py
+++ Config/Config.py
@@ -1,8 +1,9 @@
-from qfluentwidgets import QConfig, ConfigItem, BoolValidator, qconfig
+from qfluentwidgets import QConfig, ConfigItem, BoolValidator, qconfig, Theme
 
 class Config(QConfig):
 	enableAcrylicBackgroundCard = ConfigItem('personalGroup', 'enableAcrylicBackgroundCard', True, BoolValidator())
 # themeModeCard = ConfigItem('personalGroup', 'themeModeCard', True, BoolValidator())
 
 config = Config()
+config.themeMode.value = Theme.DARK
 qconfig.load('Config/config.json', config)
--- PageInterfaces/SettingInterface.py
+++ PageInterfaces/SettingInterface.py
@@ -1,5 +1,5 @@
 from PyQt5.QtWidgets import QFrame, QWidget, QLabel, QHBoxLayout, QVBoxLayout
-from qfluentwidgets import ExpandLayout, SwitchSettingCard, SettingCardGroup, FluentIcon, OptionsSettingCard, ComboBoxSettingCard, LargeTitleLabel, qconfig, QConfig, ConfigItem, BoolValidator, OptionsConfigItem, OptionsValidator, ConfigSerializer, setTheme, Theme, toggleTheme, isDarkTheme
+from qfluentwidgets import ExpandLayout, SwitchSettingCard, SettingCardGroup, FluentIcon, OptionsSettingCard, ComboBoxSettingCard, LargeTitleLabel, qconfig, QConfig, ConfigItem, BoolValidator, OptionsConfigItem, OptionsValidator, ConfigSerializer, setTheme, Theme, toggleTheme, isDarkTheme, CustomColorSettingCard
 
 from Config.Config import config
 
@@ -16,19 +16,46 @@ class SettingInterface(QFrame):
 
 		self.personalGroup = SettingCardGroup('个性化', self.scrollWidget)
 		self.enableAcrylicBackgroundCard = SwitchSettingCard(FluentIcon.TRANSPARENT, '亚克力效果', '使用亚克力效果', configItem=config.enableAcrylicBackgroundCard, parent=self.personalGroup)
-		self.themeModeCard = OptionsSettingCard(config.themeMode, FluentIcon.BRUSH, '主题模式', '改变软件的主题模式', texts=['暗色', '亮色', '跟随系统设置'], parent=self.personalGroup)
+		# self.themeModeCard = OptionsSettingCard(config.themeMode, FluentIcon.BRUSH, '主题模式', '改变软件的主题模式', texts=['暗色', '亮色', '跟随系统设置'], parent=self.personalGroup)
+		self.themeCard = OptionsSettingCard(
+				config.themeMode,
+				FluentIcon.BRUSH,
+				self.tr('Application theme'),
+				self.tr("Change the appearance of your application"),
+				texts=[
+					self.tr('Light'), self.tr('Dark'),
+					self.tr('Use system setting')
+				],
+				parent=self.personalGroup
+		)
+		self.themeColorCard = CustomColorSettingCard(
+				config.themeColor,
+				FluentIcon.PALETTE,
+				self.tr('Theme color'),
+				self.tr('Change the theme color of you application'),
+				self.personalGroup
+		)
+
 
 		self.personalGroup.addSettingCard(self.enableAcrylicBackgroundCard)
-		self.personalGroup.addSettingCard(self.themeModeCard)
+		# self.personalGroup.addSettingCard(self.themeModeCard)
+		self.personalGroup.addSettingCard(self.themeCard)
+		self.personalGroup.addSettingCard(self.themeColorCard)
 
 		self.expandLayout.addWidget(self.personalGroup)
 
 		self.hBoxLayout.addWidget(self.settingLabel)
 		self.hBoxLayout.addWidget(self.scrollWidget)
 
+		self.__connectSignalToSlot()
+
 	def __onThemeModo(self, theme=Theme):
 		setTheme(Theme.LIGHT)
 
 	def __connectSignalToSlot(self):
-		self.enableAcrylicCard.checkedChanged.connect(self.acrylicEnableChanged)
+		# self.enableAcrylicCard.checkedChanged.connect(self.acrylicEnableChanged)
+
+		self.themeCard.optionChanged.connect(lambda ci: setTheme(config.get(ci)))
+		# self.themeColorCard.colorChanged.connect(lambda c: setThemeColor(c))
+
 		config.themeChanged.connect(self.__onThemeModo)
